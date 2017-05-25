95df423ef9f993e59f973ca0909d9709daa45b1e
rk <rick.kowaleski@gmail.com>
2017-05-24 21:00:00 -0600

    Introduce ng-bootstrap

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{name}}</h1>
+    <small class="text-muted">I am using ng-bootstrap!!!</small>
+  `,
 })
 export class AppComponent  { name = 'World'; }
